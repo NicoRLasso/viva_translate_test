33ffd5509346d643e513fb25a3b40c623b20d497
Nicolas <nicolas.rlasso@gmail.com>
2023-03-03 21:58:48 -0500

    Fix participates

--- viva_translate/participants/views.py
+++ viva_translate/participants/views.py
@@ -1,14 +1,15 @@
-from viva_translate.participants.models import Participants
-from viva_translate.participants.serializers import ParticipantSerializer
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
-class UserViewSet(mixins.CreateModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   GenericViewSet):
-    """
-    A viewset for viewing and editing Participants instances.
-    """
+from viva_translate.participants.models import Participants
+from viva_translate.participants.serializers import ParticipantSerializer
+
+
+class ParticipantViewSet(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
     serializer_class = ParticipantSerializer
     queryset = Participants.objects.all()
